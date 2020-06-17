2ebe5e0e15cfdeedea3200bea14208bfe91fca2c
gx1999 <aaron131402@gmail.com>
2020-06-17 17:57:31 +0800

    1

--- app/src/main/java/gx/upc/edu/gittest/MainActivity.java
+++ app/src/main/java/gx/upc/edu/gittest/MainActivity.java
@@ -1,15 +1,59 @@
 package gx.upc.edu.gittest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentTransaction transaction;
+    private FragmentManager fragmentManager;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            fragmentManager = getSupportFragmentManager();
+            transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.item1:
+                    transaction.replace(R.id.content,new Fragment1());
+                    transaction.commit();
+                    return true;
+                case R.id.item2:
+                    transaction.replace(R.id.content,new Fragment2());
+                    transaction.commit();
+                    return true;
+                case R.id.item3:
+                    transaction.replace(R.id.content,new Fragment2());
+                    transaction.commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDefaultFragment();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
+    private void setDefaultFragment() {            // set initial fragment
+        fragmentManager = getSupportFragmentManager();
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, new Fragment1());
+        transaction.commit();
+    }
 }
